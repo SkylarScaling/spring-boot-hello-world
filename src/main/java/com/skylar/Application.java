244bb9dc7df983ebb02f7ff6fa427c596f316417
Skylar Scaling <sscaling@redhat.com>
2024-10-01 10:07:28 -0600

    refactor from example

--- src/main/java/com/skylar/Application.java
+++ src/main/java/com/skylar/Application.java
@@ -1,16 +1,8 @@
-package com.edw;
+package com.skylar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * <pre>
- *     com.edw.Application
- * </pre>
- *
- * @author Muhammad Edwin < edwin at redhat dot com >
- * 23 Apr 2020 14:00
- */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
